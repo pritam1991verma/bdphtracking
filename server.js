263f15034361042c166bfe24b71af1e7fd50abcb
Pritam Kumar <pritam@bdph.in>
2026-05-13 01:35:40 +0530

    fixed syntax error

--- server.js
+++ server.js
@@ -2234,6 +2234,4 @@ const PORT = process.env.PORT || 3000;
 
 server.listen(PORT, () => {
   console.log(`Server running on port ${PORT}`);
-});, () => {
-  console.log("Running on http://localhost:3000");
 });
