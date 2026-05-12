28ae967117ff4cf6eca8fc5cce10ac37a1dbf7b9
Pritam Kumar <pritam@bdph.in>
2026-05-13 01:30:52 +0530

    render port fix

--- server.js
+++ server.js
@@ -2230,6 +2230,10 @@ app.get("/", (_req, res) => {
   res.sendFile(`${__dirname}/public/login.html`);
 });
 
-server.listen(3000, () => {
+const PORT = process.env.PORT || 3000;
+
+server.listen(PORT, () => {
+  console.log(`Server running on port ${PORT}`);
+});, () => {
   console.log("Running on http://localhost:3000");
 });
